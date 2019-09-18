ab7266dee9a9dbc582affaccf70e5e2548ff7790
samgdotson <sgd2@illinois.edu>
2019-09-18 11:28:13 -0500

    processed weather data

--- weather/data_proc.py
+++ weather/data_proc.py
@@ -1,35 +1,62 @@
 import pandas as pd
 import numpy as np 
+import datetime as dt
 import matplotlib.pyplot as plt 
 
-# import weather data
+# import raw data -- the raw data is available for download on the box website.
 path = "/home/dotson/Documents/cairo/willard_weather_data.csv"
 
-raw_data = pd.read_csv(path, parse_dates=['DATE'], index_col=['DATE'], usecols=[1, 43, 44])
-raw_data.plot()
+weather_df = pd.read_csv(path, usecols=[1, 43, 44],) 
+weather_df = weather_df.fillna(-999.99) # fill NaN values with -999+
 
+# convert to datetime
+weather_df['DATE'] = pd.to_datetime(weather_df['DATE'])
+
+# simplify column names
+weather_df.rename(columns = {'HourlyDryBulbTemperature':'TEMP', 'HourlyPrecipitation':'PREC'}, inplace=True)
+
+print(weather_df.head(5))
+
+
+# convert temperatures to float, must use this method because there items like '47s'
+for index, temp in enumerate(weather_df['TEMP']):
+	if (type(temp) == str):
+		string = temp
+		temp_float = [float(i) for i in string.split() if i.isdigit()]
+		if len(temp_float) > 0:
+			weather_df.at[index,'TEMP'] = temp_float[0]
+		else:
+			weather_df.at[index,'TEMP'] = -999.99
+
+# # converts all to float
+weather_df['TEMP'] = weather_df['TEMP'].astype(float)
+
+# # separates the date and time stamps, not sure if this is useful. 
+# date, time = zip(*[(d.date(), d.time()) for d in weather_df['DATE']])
+# weather_df = weather_df.assign(DATE=date, time=time)
+
+# temp_df = weather_df[['DATE', 'TEMP', 'time']]
+temp_df = weather_df[['DATE', 'TEMP']]
+temp_df = temp_df[temp_df.TEMP != -999.99]
+temp_df.plot(x='DATE', y='TEMP')
+plt.show()
+
+
+# ===================================================================
+# Export cleaned datasets
+# ===================================================================
+dir_path = "/home/dotson/Documents/cairo/"
+temp_df.to_csv(dir_path + "willard_temp.csv")
+
+
+# ===================================================================
 # Figure out which columns to use
+# ===================================================================
 
-# keys = raw_data.keys()
+# keys = weather_df.keys()
 # i = 0
 # print(keys[1])
 # for key in keys:
 # 	if ("Temperature" and "Hourly") in key:
 # 		print(i, key)
 	# i +=1
-
-print(raw_data.head(30))
-# fill NaN values with -999
-# raw_data.iloc[:,0] = pd.to_datetime(raw_data.iloc[:,0])
-raw_data = raw_data.fillna(-999.99)
-
-x = raw_data.iloc[:,0]
-y = raw_data.iloc[:,1]
-# plt.plot(x,y)
-# plt.show()
-# raw_data.plot(x=DATE, y=HourlyDryBulbTemperature, kind="line")
-
-print(raw_data.head(30))
-
-
-
