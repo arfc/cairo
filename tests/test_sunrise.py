dfb4ff30a1ccca62baf5920337d7e7638b0e0c4f
Samuel Dotson <sgd2@illinois.edu>
2020-10-01 17:53:16 -0500

    merge conf?

--- tests/test_sunrise.py
+++ tests/test_sunrise.py
@@ -1,14 +1,16 @@
 from sunrise import frac_year
 import numpy as np
 
-def test_frac_year1():
+def test_frac_year_case1():
     """
     Tests if year fraction is correct for
     midnight on January 1st.
     """
 
-def test_frac_year1():
+def test_frac_year_case2():
     """
     Tests if year fraction is correct for
     midnight on January 1st.
     """
+
+    # assert()
