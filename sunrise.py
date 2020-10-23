177b8f6b1c35abb23ba745a3d3bfa00fb560a729
Sam Dotson <44342873+samgdotson@users.noreply.github.com>
2020-10-23 11:04:07 -0500

    Update sunrise.py

--- sunrise.py
+++ sunrise.py
@@ -265,7 +265,6 @@ def solar_elevation(ha, delta, lat=lat):
     sin_term = np.sin(delta * np.pi / 180) * np.sin(lat * np.pi / 180)
     cos_term = np.cos(delta * np.pi / 180) * np.cos(lat * \
                       np.pi / 180) * np.cos(ha * np.pi / 180)
-    # alpha = np.arcsin((sin_term+cos_term)*np.pi/180)
     alpha = np.arcsin((sin_term + cos_term)) * 180 / np.pi
 
     return alpha
