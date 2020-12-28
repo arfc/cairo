expand tests and examples, Sam pointed out that I was using the wrong order for the lorenz96 and 63 examples and tests

lorenz.py
@@ -13,9 +13,9 @@ def lorenz96(x, t, N=5, F=8):
 
     Parameters
     ----------
-    x : numpy array
+    x : numpy array or list
         The independent variable
-    t : numpy array
+    t : numpy array or list
         The time of the simulation
     N : integer
         The number of variables in the system
@@ -43,7 +43,7 @@ def generate_L96(t, P=0.01, N=5, F=8):
 
     Parameters
     ----------
-    t : numpy array
+    t : numpy array or list
         The array of time steps.
     P : integer or float
         The initial perturbation on the system.
@@ -77,22 +77,28 @@ def lorenz63(x, t, rho=28.0, sigma=10.0, beta=(8.0 / 3.0)):
     
     Parameters
     ----------
-    x : numpy array
+    x : numpy array or list
         The independent variable
-    t : numpy array
+    t : numpy array or list
         The array of time steps.
     rho : integer or float
-        The _______
+        Dimensionless quantity related to the Prandtl
+        and Rayleigh numbers.
+        For rho < 1 there is one equilibrium point
+        at the origin. There is a bifurcation when
+        rho = 1, and when rho > 1 there are two
+        more critical.
     sigma : integer or float
-        The _______
+        Dimensionless quantity related to the Prandtl
+        and Rayleigh numbers.
     beta : integer or float
-        The _______
+        Dimensionless quantity related to the Prandtl
+        and Rayleigh numbers.
     
     Returns
     -------
     dxdt : The function
     """
-
     N = 3
 
     dxdt = np.ones(N)
@@ -115,14 +121,21 @@ def generate_L63(t, rho=28.0, sigma=10.0, beta=(8.0 / 3.0)):
     
     Parameters
     ----------
-    t: numpy array
+    t: numpy array or list
         The array of time steps
     rho : integer or float
-        The ______
+        Dimensionless quantity related to the Prandtl
+        and Rayleigh numbers.
+        For rho < 1 there is one equilibrium point
+        at the origin. There is a bifurcation when
+        rho = 1, and when rho > 1 there are two
+        more critical.
     sigma : integer or float
-        The ______
+        Dimensionless quantity related to the Prandtl
+        and Rayleigh numbers.
     beta : integer or float
-        The ______
+        Dimensionless quantity related to the Prandtl
+        and Rayleigh numbers.
     
     Returns
     -------

tests/test_lorenz.py
@@ -12,22 +12,9 @@ def test_lorenz96_constant():
     
     Not finished
     """
-    obs_i = 0
-    exp_i = lorenz96(x, t, 4, 0)
-    assert obs_i == exp_i
-
-
-def test_lorenz96_constant():
-    """
-    For the case of a horizontal distribution,
-    so that the slope should be a zero over
-    time.
-    
-    Not finished
-    """
-    obs_i = 0
-    exp_i = lorenz96(x, t, 5, 8)
-    assert obs_i == exp_i
+    obs_i = np.array([8., 8., 8., 8., 8.])
+    exp_i = lorenz96(np.array([0,0,0,0,0]), np.arange(0, 4, 1))
+    assert (obs_i == exp_i).all()
 
 
 def test_generate_L96_zero():
@@ -41,7 +28,7 @@ def test_generate_L96_zero():
     [0., 0., 0., 0.],[0., 0., 0., 0.],
     [0., 0., 0., 0.]])
     exp_i = generate_L96(np.zeros(4), 0, 4, 0)
-    assert obs_i == exp_i
+    assert (obs_i == exp_i).all()
 
 
 def test_generate_L96_constant():
@@ -55,7 +42,7 @@ def test_generate_L96_constant():
     [8.,8.,8.,8.,8.],[8.,8.,8.,8.,8.],
     [8.,8.,8.,8.,8.],[8.,8.,8.,8.,8.]])
     exp_i = generate_L96(np.ones(5),0,5,8)
-    assert obs_i == exp_i
+    assert (obs_i == exp_i).all()
 
 
 def test_generate_L96_perturbed():
@@ -69,7 +56,7 @@ def test_generate_L96_perturbed():
     [9.,8.,8.,8.,8.],[9.,8.,8.,8.,8.],
     [9.,8.,8.,8.,8.],[9.,8.,8.,8.,8.]])
     exp_i = generate_L96(np.ones(5),1,5,8)
-    assert obs_i == exp_i
+    assert (obs_i == exp_i).all()
 
 
 def test_lorenz63():
@@ -78,13 +65,53 @@ def test_lorenz63():
     """
     obs_i = 0
     exp_i = 0
-    assert obs_i == exp_i
+    assert (obs_i == exp_i).all()
 
 
-def test_generate_L63():
+def test_generate_L63_rho_leq1():
     """
     
     """
-    obs_i = 0
-    exp_i = 0
-    assert obs_i == exp_i
+    obs_i = np.array([[1.        , 1.        , 1.        ],
+       [0.40779223, 0.37990175, 0.16719831],
+       [0.22938198, 0.21751446, 0.03659244],
+       [0.13924149, 0.13244845, 0.01146649]])
+    exp_i = generate_L63(np.arange(0,4,1),0.5)
+    assert obs_i == approx(exp_i)
+
+
+def test_generate_L63_rho_geq1():
+    """
+
+    """
+    obs_i = np.array([[ 1.        ,  1.        ,  1.        ],
+           [-9.37856995, -8.35703373, 29.36232527],
+           [-8.17349956, -9.56202269, 24.62070256],
+           [-7.45666031, -6.19099807, 27.44180888]])
+    exp_i = generate_L63(np.arange(0,4,1))
+    assert obs_i == approx(exp_i)
+
+
+def test_generate_L63_rho_eq1():
+    """
+    Over a small range of inputs,
+    check that
+    """
+    obs_i = np.array([[1.        , 1.        , 1.        ],
+       [0.63113547, 0.6153045 , 0.23992849],
+       [0.53483921, 0.5285069 , 0.12377577],
+       [0.4835722 , 0.47931319, 0.09419961]])
+    exp_i = generate_L63(np.arange(0,4,1),1)
+    assert obs_i == approx(exp_i)
+
+
+def test_generate_L63_zeros():
+    """
+    Generate a set with an input of zeros
+    """
+    obs_i = np.array([[1., 1., 1.],
+           [1., 1., 1.],
+           [1., 1., 1.],
+           [1., 1., 1.]])
+    exp_i = generate_L63(np.zeros(4))
+    assert (obs_i == exp_i).all()